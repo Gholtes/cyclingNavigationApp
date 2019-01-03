03b54136462f789460bb8def71632f073d5bff51
Grant Holtes <gwholtes@gmail.com>
2019-01-03 13:42:35 +1300

    Add files via upload

---
+++ Crow Flies v1Tests/Crow_Flies_v1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Crow_Flies_v1Tests.swift
+//  Crow Flies v1Tests
+//
+//  Created by Grant Holtes on 28/10/18.
+//  Copyright © 2018 Grant Holtes. All rights reserved.
+//
+
+import XCTest
+@testable import Crow_Flies_v1
+
+class Crow_Flies_v1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
